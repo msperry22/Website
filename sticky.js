706999c949748bc052ce99a24a5459cfaee0026a
msperry22 <45888621+msperry22@users.noreply.github.com>
2018-12-14 21:41:06 -0500

    Add files via upload

---
+++ sticky.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {myFunction()};
+
+var topnav = document.getElementByClass("topnav");
+var sticky = topnav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
